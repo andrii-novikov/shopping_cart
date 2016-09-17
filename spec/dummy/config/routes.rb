5c6a030c506c4c9b076133b0663a4f2d6f17e651
Andrew Novikov <andruha2921@gmail.com>
2016-09-17 16:03:36 +0300

    setup dummy project

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  devise_for :users
   mount ShoppingCart::Engine => "/shopping_cart"
+  get 'home/index'
 end
---
+++ spec/dummy/db/seeds.rb
@@ -0,0 +1,4 @@
+Product.create(title:'Product 1')
+Product.create(title:'Product 2')
+Product.create(title:'Product 3')
+Product.create(title:'Product 4')
